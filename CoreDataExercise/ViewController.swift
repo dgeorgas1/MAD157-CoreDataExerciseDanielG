f78f07e6b8d9d3c5d668eea7eede75f845ebeac5
Daniel Georgas <dgeorgas@students.mchenry.edu>
2024-03-10 19:27:31 -0500

    Commit changes

--- CoreDataExercise/ViewController.swift
+++ CoreDataExercise/ViewController.swift
@@ -6,14 +6,78 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var typeRecordTF: UITextField!
+    @IBOutlet weak var displayRecordLabel: UILabel!
+    
+    var dataManager: NSManagedObjectContext!
+    var listArray = [NSManagedObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        dataManager = appDelegate.persistentContainer.viewContext
+        displayRecordLabel.text?.removeAll()
+        fetchData()
     }
-
-
+    
+    func fetchData() {
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Item")
+        
+        do {
+            let result = try dataManager.fetch(fetchRequest)
+            
+            listArray = result as! [NSManagedObject]
+            
+            for item in listArray {
+                let product = item.value(forKey: "about") as! String
+                
+                displayRecordLabel.text! += product
+            }
+        }
+        catch {
+            print("Error retrieving the data")
+        }
+    }
+    
+    @IBAction func deleteBtn(_ sender: UIButton) {
+        let deleteItem = typeRecordTF.text!
+        
+        for item in listArray {
+            if item.value(forKey: "about") as! String == deleteItem {
+                dataManager.delete(item)
+            }
+            
+            do {
+                try self.dataManager.save()
+            }
+            catch {
+                print("Error deleting the data")
+            }
+            displayRecordLabel.text?.removeAll()
+            typeRecordTF.text?.removeAll()
+            fetchData()
+        }
+    }
+    
+    @IBAction func saveBtn(_ sender: UIButton) {
+        let newEntity = NSEntityDescription.insertNewObject(forEntityName: "Item", into: dataManager)
+        
+        newEntity.setValue(typeRecordTF.text!, forKey: "about")
+        
+        do {
+            try self.dataManager.save()
+            listArray.append(newEntity)
+        }
+        catch {
+            print("Error saving the data")
+        }
+        displayRecordLabel.text?.removeAll()
+        typeRecordTF.text?.removeAll()
+        fetchData()
+    }
 }
-
